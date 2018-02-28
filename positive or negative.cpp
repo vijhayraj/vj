b908df9dc68c608394c09b59537a3185502f4a93
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-02-28 17:54:39 +0530

    Update positive or negative.cpp

--- positive or negative.cpp
+++ positive or negative.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
- 
-int main()
+int main() {
+int a;
+cout<<"enter the value\n";
+cin>>a;
+if(a>0)
 {
-    signed long num1 = 0;
-	cout << "\n\n Check whether a number is positive, negative or zero :\n";
-	cout << "-----------------------------------------------------------\n";  
-	cout << " Input a number : ";
-    cin >> num1;
-    if(num1 > 0)
-    {
-        cout << " The entered number is positive.\n\n";
-    }
-    else if(num1 < 0)
-    {
-        cout << " The entered number is negative.\n\n";
-    }
-    else
-    {
-        std::cout << " The number is zero.\n\n";
-    }
-    return 0;
+	cout<<"the number is positive";
+}
+else if(a<0)
+{
+	cout<<"the number is negative";
+}
+else
+{
+	cout<<"Zero";
+}
+	return 0;
 }
